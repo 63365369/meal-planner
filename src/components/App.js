5d7385f37d68b24be5dd82ddff19f740d7d57688
63365369 <63365369@qq.com>
2017-12-06 09:54:59 +0800

    connect
    mapStateToProps，mapDispatchToProps

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,13 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { addRecipe , removeFromCalendar} from '../actions'
 
 class App extends Component {
+    doThing = ()=>{
+        this.props.selectRecipe({})
+    }
     render() {
+        console.log('Props',this.props)
         return (
             <div>
                 hello world
@@ -10,4 +16,24 @@ class App extends Component {
     }
 }
 
-export default App
+function mapStateToProps(calendar) {
+    const dayOrder = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday']
+
+    return {
+        calendar: dayOrder.map((day) => ({
+            day,
+            meals: Object.assign({}, calendar[day])
+        })),
+    }
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        selectRecipe: (data) => dispatch(addRecipe(data)),
+        remove: (data) => dispatch(removeFromCalendar(data))
+    }
+}
+
+export default connect(
+    mapStateToProps,mapDispatchToProps
+)(App)
